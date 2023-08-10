47d59dab78bb2397a935bf4b6c11398e27b81331
ilhamkukuh <ilham@pandatech.io>
2023-08-10 17:45:29 +0700

    chore: comment api faq

--- src/services/faq/faq.api.ts
+++ src/services/faq/faq.api.ts
@@ -1,14 +1,14 @@
-import { AxiosResponse } from "axios";
+// import { AxiosResponse } from "axios";
 
-import { axiosInstance } from "@/configs/axios";
-import { DataResponseType } from "@/interfaces/response";
+// import { axiosInstance } from "@/configs/axios";
+// import { DataResponseType } from "@/interfaces/response";
 
-export const faqServices = {
-  getFaqs: async () => {
-    const response: AxiosResponse<DataResponseType<>> = await axiosInstance({
-      url: "/faq",
-      method: "get",
-    });
-    return response.data;
-  },
-};
+// export const faqServices = {
+//   getFaqs: async () => {
+//     const response: AxiosResponse<DataResponseType<>> = await axiosInstance({
+//       url: "/faq",
+//       method: "get",
+//     });
+//     return response.data;
+//   },
+// };
